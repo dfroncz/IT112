52f7f6cc8815a7ac610a6c35e1f645891b737e05
dfroncz <dfroncz@gmail.com>
2021-05-13 19:19:22 -0700

    Models Commit ver2

--- pythonclub/club/models.py
+++ pythonclub/club/models.py
@@ -10,11 +10,11 @@ from django.contrib.auth.models import User
 
 
 class Meeting(models.Model):
-    meeting_title=()
-    meeting_date=()
-    meeting_time=()
-    location=()
-    agenda=()
+    meeting_title=models.CharField(max_length=255, null=True)
+    meeting_date=models.DateField(null=True)
+    meeting_time=models.DateField(null=True)
+    location=models.CharField(max_length=255, null=True)
+    agenda=models.TextField(null=True)
 
     def __str__(self):
         return self.meeting_title
@@ -23,9 +23,9 @@ class Meeting(models.Model):
         db_table='Meeting'
 
 class Meeting_Minutes(models.Model):
-    meeting_id=()
-    attendance=()
-    minutes_text=()
+    meeting_id=models.ForeignKey(Meeting, on_delete=models.DO_NOTHING, null=True)
+    attendance=models.ManyToManyField(User)
+    minutes_text=models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.meeting_id
@@ -34,12 +34,12 @@ class Meeting_Minutes(models.Model):
         db_table='Meeting_Minutes'
 
 class Resource(models.Model):
-    resource_name=()
-    resource_type=()
-    URL=()
-    date_entered=()
-    user_id=()
-    description=()
+    resource_name=models.CharField(max_length=255, null=True)
+    resource_type=models.CharField(max_length=255, null=True)
+    URL=models.URLField(null=True)
+    date_entered=models.DateField(null=True)
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    description=models.TextField(null=True)
 
 
     def __str__(self):
@@ -49,12 +49,12 @@ class Resource(models.Model):
         db_table='Resource'
 
 class Event(models.Model):
-    event_title=()
-    location=()
-    date=()
-    time=()
-    description=()
-    user_id=()
+    event_title=models.CharField(max_length=255, null=True)
+    location=models.CharField(max_length=255, null=True)
+    date=models.DateField(null=True)
+    time=models.DateField(null=True)
+    description=models.TextField(null=True)
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     
 
     def __str__(self):
