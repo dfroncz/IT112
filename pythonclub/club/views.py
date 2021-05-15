aa6c4e8cac83b68b90c328a7ab839574712e86cf
dfroncz <dfroncz@gmail.com>
2021-05-14 22:40:41 -0700

    Second Meeting View Commit

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, Meeting_Minutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def meetings(request):
+    meeting_title=Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meeting_title': meeting_title})
