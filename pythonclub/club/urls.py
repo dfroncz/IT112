947b6edec31837b2c89c212f955fa1e13f7d2f81
dfroncz <dfroncz@gmail.com>
2021-05-25 23:28:26 -0700

    Detail Commit

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('meetings/', views.meetings, name='meetings'),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='detail'),
 ]
--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, Meeting_Minutes, Resource, Event
 
 # Create your views here.
@@ -8,3 +8,7 @@ def index(request):
 def meetings(request):
     meeting_title=Meeting.objects.all()
     return render(request, 'club/meetings.html', {'meeting_title': meeting_title})
+
+def meetingdetail(request, id): 
+    meeting=get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/meetingdetail.html', {'meeting' : meeting})
